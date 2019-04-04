68b29ff7f8ffde0fa5f6089f1f6fef99af715787
Alex McGrath <45976293+alexmmcgrath@users.noreply.github.com>
2019-04-04 14:51:35 -0400

    Added Compass_Script and Fixed Magnet_Script

---
+++ 2D Magnets/Assets/Compass_Script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Compass : MonoBehaviour
+{
+
+    public Vector3 point_N;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 point_N = new Vector3(transform.position.x - 0.375f, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Creation direction = GetComponent<Creation>();
+        var vec_pointing = (point_N - transform.position);
+        var vec_direct = (vec_pointing / vec_pointing.magnitude);
+
+        float angle = Vector3.Angle(vec_direct, direction.fin_direct);
+
+
+        if(angle == 0)
+        {
+            
+        }
+        else if(angle == 180)
+        {
+            
+        }
+        else
+        {
+
+        }
+
+    }
+}
---
+++ 2D Magnets/Assets/Magnet_Script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creation : MonoBehaviour
+{
+    
+    public Vector3 North;
+    public Vector3 South;
+    //public Vector3 Capsule_position = new Vector3(1.5f, 0, 0);  // set center of sphere for the start and end of capsule
+
+    public LayerMask FieldLines;
+
+    public Vector3 fin_direct;
+
+    void Start()
+    {
+       North = new Vector3(transform.position.x - 1.75f, transform.position.y, transform.position.z);
+       South = new Vector3(transform.position.x + 1.75f, transform.position.y, transform.position.z);
+     
+       
+    }
+    void Update()
+    {
+        Collider[] colliders = Physics.OverlapCapsule(North, South, 2.5f, FieldLines);
+
+        foreach(var collider in colliders)
+        {
+            Rigidbody comp = collider.GetComponent<Rigidbody>();
+            var attractor = (North - comp.position);
+            var direction_a = (attractor / attractor.magnitude);
+            var repulse = (comp.position - South);
+            var direction_s = (repulse / repulse.magnitude);
+            var attra_str = (1 / (attractor.sqrMagnitude));
+            var repul_str = (1 / (repulse.sqrMagnitude));
+
+            fin_direct = ((attra_str * attractor) + (repul_str * repulse));
+
+        }
+
+    }
+
+    Vector3 check()
+    {
+
+    }
+
+
+}
